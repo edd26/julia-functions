8bbb626053627548d967260d235e09c656e013a4
edd26 <ed18abc@herts.ac.uk>
2019-10-18 10:48:01 +0100

    Exportated  image rotation to new file

--- VideoProcessing.jl
+++ VideoProcessing.jl
@@ -232,25 +232,6 @@ end
 
 
 """
-    rotate_img_around_center(img, angle = 5pi/6)
-
-Function rotates a single image (or a frame) around the center of the image by
-@angle radians.
-"""
-function rotate_img_around_center(img, angle = 5pi/6)
-  θ = angle
-  rot = recenter(RotMatrix(θ), [size(img)...] .÷ 2)  # a rotation around the center
-  x_translation = 0
-  y_translation = 0
-  tform = rot ∘ Translation(y_translation, x_translation)
-  img2 = warp(img, rot, axes(img))
-
-  return img2
-end
-
-
-
-"""
     rotate_vid_around_center(img, rotation = 5pi/6)
 
 Function rotates a video around the center of the image by @rotation radians and
