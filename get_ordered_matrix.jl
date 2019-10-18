3ca98eb5409805a06c0c9419aabf9ca48ecdd997
edd26 <ed18abc@herts.ac.uk>
2019-10-18 15:43:36 +0100

    removed normalization, fixed min value assignment

--- get_ordered_matrix.jl
+++ get_ordered_matrix.jl
@@ -2,16 +2,17 @@
 entries representing order of values within all matrix"""
 function get_ordered_matrix(symmetric_matrix)
     data_copy = copy(symmetric_matrix)
-    data_copy .+= abs(findmin(data_copy)[1])
-    data_copy ./= abs(findmax(data_copy)[1])
+    # data_copy .+= abs(findmin(data_copy)[1])
+    # data_copy ./= abs(findmax(data_copy)[1])
     ordered_matrix = zeros(Int, size(data_copy))
+    min_value = -Inf
 
     # repetitions is equal to number of elements above diagonal= (n*(n-2))/2
     repetitions = ceil((size(data_copy)[1] * (size(data_copy)[1]-1))/2)
     for k=1:repetitions
         value, index = findmax(data_copy)
-        data_copy[index[1], index[2]] = 0
-        data_copy[index[2], index[1]] = 0
+        data_copy[index[1], index[2]] = min_value
+        data_copy[index[2], index[1]] = min_value
         ordered_matrix[index[1], index[2]] = k
         ordered_matrix[index[2], index[1]] = k
     end
