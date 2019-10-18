5a0e5160a251c0270ee7d80bd7a397dd2e2799f5
edd26 <ed18abc@herts.ac.uk>
2019-10-18 10:53:25 +0100

    Exported shift of the matrix

--- VideoProcessing.jl
+++ VideoProcessing.jl
@@ -342,23 +342,6 @@ function get_local_centers(points_per_dim, video_dimensions, shift=0, sub_img_si
 end
 
 
-"""
-    shift_to_non_negative(matrix)
-
-Returns a matrix in which values are non-negative. This is done by finding the
-minimal value in the input matrix and adding its absolute value to the matix
-elements.
-"""
-function shift_to_non_negative(matrix)
-
-    min_val = findmin(matrix)[1]
-    if min_val < 0
-        return matrix .-= min_val
-    else
-        return matrix
-    end
-end
-
 
 """
     plotimg(matrix_to_plot)
