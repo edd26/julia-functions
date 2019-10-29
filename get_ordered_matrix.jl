f19677bda71dfc967d1467dd2ca853c1c2da3fe2
edd26 <ed18abc@herts.ac.uk>
2019-10-29 14:28:53 +0000

    Refacotred documentation; added example to dcumentation

--- get_ordered_matrix.jl
+++ get_ordered_matrix.jl
@@ -1,10 +1,27 @@
 using LinearAlgebra
 
-""" Takes a symmetrc matrix and returns ordered form of this matrix.
+"""
+    get_ordered_matrix(input_matrix)
+
+Takes a `input_matrix` and returns ordered form of this matrix.
 The values of returned matrix represent position in descending ordering of
-of input matrix."""
-function get_ordered_matrix(symmetric_matrix)
-    data_copy = copy(symmetric_matrix)
+of input matrix.
+
+If `input_matrix` is symmetric, then the returned values are ordered above the
+diagonal. Lower diagonal is symetrically copied from values aboce diagonal.
+
+# Examples
+```julia-repl
+julia> a = [0 11 12; 11 0 13; 12 13 0];
+julia> get_ordered_matrix(a)
+3×3 Array{Int64,2}:
+ 0  3  2
+ 3  0  1
+ 2  1  0
+```
+"""
+function get_ordered_matrix(input_matrix)
+    data_copy = copy(input_matrix)
     if issymmetric(data_copy)
         symetry_order = true
     else
@@ -17,7 +34,7 @@ function get_ordered_matrix(symmetric_matrix)
     min_value = -Inf
 
     # repetitions is equal to number of elements above diagonal= (n*(n-2))/2
-    repetitions = ceil((size(data_copy)[1] * (size(data_copy)[1]-1))/2)
+    repetitions = Int(ceil((size(data_copy)[1] * (size(data_copy)[1]-1))/2))
     for k=1:repetitions
         value, index = findmax(data_copy)
         data_copy[index[1], index[2]] = min_value
@@ -29,7 +46,7 @@ function get_ordered_matrix(symmetric_matrix)
     end
     # ordered_matrix .*= (-1)
     # findmax(ordered_matrix)
-    @info "Original maximal value was at position: " (findmax(symmetric_matrix)[2])
+    @info "Original maximal value was at position: " (findmax(input_matrix)[2])
     @info "After ordering the first index value is at position: " (findall(x->x==1,ordered_matrix)[1])
     @info "\n"
 
