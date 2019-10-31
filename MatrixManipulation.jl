3885c8e73be3ffd0798f702cba888c8f8e7905aa
edd26 <ed18abc@herts.ac.uk>
2019-10-31 12:40:32 +0000

    Moved contetnt to module

--- MatrixManipulation.jl
+++ MatrixManipulation.jl
@@ -1,6 +1,24 @@
 using LinearAlgebra
 
 """
+    shift_to_non_negative(matrix)
+
+Returns a matrix in which values are non-negative. This is done by finding the
+minimal value in the input matrix and adding its absolute value to the matix
+elements.
+"""
+function shift_to_non_negative(matrix)
+
+    min_val = findmin(matrix)[1]
+    if min_val < 0
+        return matrix .-= min_val
+    else
+        return matrix
+    end
+end
+
+
+"""
     get_ordered_matrix(input_matrix)
 
 Takes a `input_matrix` and returns ordered form of this matrix.
