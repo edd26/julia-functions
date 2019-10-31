bd30640b743a3d6ef940dde5143fafee7ee023fb
edd26 <ed18abc@herts.ac.uk>
2019-10-31 12:49:54 +0000

    Moved local gradients to module

--- ImageProcessing.jl
+++ ImageProcessing.jl
@@ -14,3 +14,38 @@ function rotate_img_around_center(img, angle = 5pi/6)
 
   return img2
 end
+
+
+"""
+    get_local_gradients(video_array, centers, sub_img_size)
+
+Computes the gradients in the subimage, takes the mean of sum of absolute
+values of both hotizontal and vertical gradients as a representative of a
+subimage.
+"""
+function get_local_gradients(video_array, centers, sub_img_size)
+    @debug "Entering get_local_gradients"
+    half_size = ceil(Int,(sub_img_size-1)/2)
+    half_range = half_size
+    h, w, len = get_video_dimension(video_array)
+    extracted_pixels = zeros(sub_img_size, sub_img_size, len)
+
+    @debug "starting frame procesing"
+    for frame = 1:len
+        img = video_array[frame]
+        img_grad = imgradients(img, KernelFactors.ando3, "replicate")
+        img_grad_abs = map(abs, img_grad[1]) + map(abs, img_grad[2])
+        for index_x = 1:size(centers,2)
+            c_x = centers[2, index_x]
+            for index_y = 1:size(centers,2)
+                c_y = centers[1, index_y]
+                sub_img  = img_grad_abs[(c_x-half_size):(c_x+half_size),
+                                        (c_y-half_size):(c_y+half_size)]
+
+                extracted_pixels[index_x, index_y, frame] =mean(sub_img)
+            end
+        end
+        # @debug "Next frame" frame
+    end
+    return extracted_pixels
+end
