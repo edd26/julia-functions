ed39ff162773c72a69983148dd3c9e445c5052ea
edd26 <ed18abc@herts.ac.uk>
2019-11-07 12:37:15 +0000

    Module for functions wrapping plotting parameters

---
+++ PlottingWrappers.jl
@@ -0,0 +1,16 @@
+"""
+    plot_square_heatmap(matrix, tick_step, tick_end;
+                                title, img_size=(900, 800), img_dpi=300)
+
+Takes matrix and plots it as a heatmap. Funtion returns the handler to the
+heatmap.
+"""
+function plot_square_heatmap(matrix, tick_step, tick_end;
+                                title, img_size=(900, 800), img_dpi=300)
+    heat_map = heatmap(matrix,  color=:lightrainbow,
+                    title="Order matrix of $(file_n), tiles size:$(sub_img_size)",
+                    size=img_size, dpi=img_dpi,
+                    ticks=0:step:end)
+
+    return heat_map
+end
