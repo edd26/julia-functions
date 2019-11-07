e43f695401e820e1e9b7d85148716b41daf6c4e3
edd26 <ed18abc@herts.ac.uk>
2019-11-07 13:17:39 +0000

    Plot title fixed

--- PlottingWrappers.jl
+++ PlottingWrappers.jl
@@ -1,14 +1,14 @@
 """
     plot_square_heatmap(matrix, tick_step, tick_end;
-                                title, img_size=(900, 800), img_dpi=300)
+                                plt_title, img_size=(900, 800), img_dpi=300)
 
 Takes matrix and plots it as a heatmap. Funtion returns the handler to the
 heatmap.
 """
 function plot_square_heatmap(matrix, tick_step, tick_end;
-                                title, img_size=(900, 800), img_dpi=300)
+                                plt_title, img_size=(900, 800), img_dpi=300)
     heat_map = heatmap(matrix,  color=:lightrainbow,
-                    title="Order matrix of $(file_n), tiles size:$(sub_img_size)",
+                    title=plt_title,
                     size=img_size, dpi=img_dpi,
                     ticks=0:tick_step:tick_end)
 
