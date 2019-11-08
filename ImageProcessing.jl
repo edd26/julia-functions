7716261a8b5c3cc003bb74c51487241112ea9769
edd26 <ed18abc@herts.ac.uk>
2019-11-08 14:53:45 +0000

    Added column parameter to rearrange_filters_arr

--- ImageProcessing.jl
+++ ImageProcessing.jl
@@ -451,7 +451,7 @@ how many of the element from `im_fiter` are displayed.
 
 `im_filters` is an array with elements of type Matrix{Gray}.
 """
-function rearrange_filters_arr(im_filter; showing_number=-1)
+function rearrange_filters_arr(im_filter; showing_number=-1, columns=-1)
     mask_size = size(masks[1],1)
     masks_num = length(masks)
     if showing_number == -1 || showing_number > masks_num
@@ -459,7 +459,10 @@ function rearrange_filters_arr(im_filter; showing_number=-1)
     else
         max_indeks = showing_number
     end
-    columns = Int(ceil(sqrt(masks_num)))
+
+    if columns == -1
+        columns = Int(ceil(sqrt(masks_num)))
+    end
     rows= Int(ceil(masks_num/columns))
 
     all_filters = zeros(Gray, rows*mask_size, columns*mask_size)
