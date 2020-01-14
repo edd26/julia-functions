b7824cdfc7182a7ea800a6856b777978471eed4c
edd26 <ed18abc@herts.ac.uk>
2020-01-14 17:51:40 +0000

    Added more options to plotting square matrix

--- PlottingWrappers.jl
+++ PlottingWrappers.jl
@@ -9,8 +9,14 @@ function plot_square_heatmap(matrix, tick_step, tick_end;
                                 plt_title, img_size=(900, 800), img_dpi=300)
     heat_map = heatmap(matrix,  color=:lightrainbow,
                     title=plt_title,
-                    size=img_size, dpi=img_dpi,
-                    ticks=0:tick_step:tick_end)
+                    size=img_size,
+                    dpi=img_dpi,
+                    lw=2,
+                    thickness_scaling=2,
+                    top_margin= -20px,
+                    left_margin=[-28px 0px],
+                    bottom_margin= -20px,
+                    ticks=0:tick_step:tick_end);
 
     return heat_map
 end
