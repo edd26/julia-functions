3ca1cd8ae9a51ea50a9309fe37ecd6c2ccb640ba
Emil Dmitruk <edmitruk@mion.elka.pw.edu.pl>
2020-01-20 16:31:22 +0000

    Introduced ploting parameters as a named tuple

--- PlottingWrappers.jl
+++ PlottingWrappers.jl
@@ -6,18 +6,18 @@ Takes matrix and plots it as a heatmap. Funtion returns the handler to the
 heatmap.
 """
 function plot_square_heatmap(matrix, tick_step, tick_end;
-                                plt_title, img_size=(900, 800), img_dpi=300)
+                                plt_title, plot_params= (dpi=300,
+                                				size=(900,800),
+                                				lw=1,
+                                				thickness_scaling=1,
+                                				top_margin= 0px,
+                                				left_margin=[0px 0px],
+                                				bottom_margin= 0px
+                                				),)
     heat_map = heatmap(matrix,  color=:lightrainbow,
                     title=plt_title,
-                    size=img_size,
-                    dpi=img_dpi,
-                    # lw=2,
-                    thickness_scaling=3,
-                    top_margin= -33px,
-                    left_margin=[-50px 0px],
-                    right_margin=[15px 0px],
-                    bottom_margin= -35px,
-                    ticks=0:tick_step:tick_end);
+                    size=plot_params.size, dpi=plot_params.dpi,
+                    ticks=0:tick_step:tick_end)
 
     return heat_map
 end
