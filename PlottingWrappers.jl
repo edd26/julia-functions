09d4b939922715d868baef1dc1ed4d0f5441bef3
edd26 <ed18abc@herts.ac.uk>
2020-02-19 14:55:49 +0000

    Moved flipping of heatmap into plotting function

--- PlottingWrappers.jl
+++ PlottingWrappers.jl
@@ -7,7 +7,8 @@ Takes matrix and plots it as a heatmap. Funtion returns the handler to the
 heatmap.
 """
 function plot_square_heatmap(matrix, tick_step, tick_end;
-                                plt_title, plot_params= (dpi=300,
+                                plt_title="", yflip_matrix=true,
+                                 plot_params= (dpi=300,
                                 				size=(900,800),
                                 				lw=1,
                                 				thickness_scaling=1,
@@ -19,6 +20,7 @@ function plot_square_heatmap(matrix, tick_step, tick_end;
                     title=plt_title,
                     size=plot_params.size, dpi=plot_params.dpi,
                     ticks=0:tick_step:tick_end)
+    yflip_matrix && plot!( yflip = true,)
 
     return heat_map
 end
