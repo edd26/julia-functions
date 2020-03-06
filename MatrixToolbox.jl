f6dd18bf35f3bbaa434ded05ccc0b4a14c8c6431
edd26 <ed18abc@herts.ac.uk>
2020-03-06 15:28:45 +0000

    Copied function for getting ordereing graph

--- MatrixToolbox.jl
+++ MatrixToolbox.jl
@@ -131,3 +131,41 @@ function save_matrix_to_file(matrix, filename)
         writedlm(io,  matrix, ',')
     end
 end
+
+
+# =====
+# Copied form Julia learning repo
+
+"""
+Returns ordering of the @geometric_matrix given as an input. If value @ascending
+is set to true, the values are number from the lowest value, to the highest. If
+false, the values are numbered from highest to the lowest.
+"""
+function generate_matrix_ordering(geometric_matrix, ascending = true)
+    matrix_size = size(geometric_matrix, 2)
+    elemnts_above_diagonal = Int((matrix_size^2-matrix_size)/2)
+    matrix_ordering = zeros(Int, 2,elemnts_above_diagonal)
+
+    A = copy(geometric_matrix)
+
+    (ascending) ? (method=findmax) : (method=findmin)
+
+    for element in 1:elemnts_above_diagonal
+    #     Find maximal distance
+        minimal_value = method(A)
+    #     Get the coordinates (only 2 dimensions, because it is distance matrix)
+        matrix_ordering[1,element] = Int(minimal_value[2][1])
+        matrix_ordering[2,element] = Int(minimal_value[2][2])
+    #
+    # #     Zero minval in A (above and below diagonal) so next minval can be found
+        A[matrix_ordering[1,element], matrix_ordering[2,element]] = 0.0
+        A[matrix_ordering[2,element], matrix_ordering[1,element]] = 0.0
+    end
+
+    # change from min to max order to the max to min order (? necessary ?)
+    if ascending
+        matrix_ordering = matrix_ordering[:,end:-1:1]
+    end
+
+    return matrix_ordering
+end
