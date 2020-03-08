f489c6788356d1a192e6558893e98e6dc9fcf05f
Emil Dmitruk <edmitruk@mion.elka.pw.edu.pl>
2020-03-08 20:01:48 +0000

    Added function for saving plot with any number of parameters

--- SavingFigures.jl
+++ SavingFigures.jl
@@ -1,19 +1,4 @@
 """
-Save figures with given parameters.
-
-Note: Exported from TestingPairwiseCorrelationmatrix.jl
-"""
-function save_figures_old(ref, path, choice, points_per_dim, tau, size_limiter)
-    name = split(choice, ".")[1]
-    name =  path * name *
-            "_size$(size_limiter)_points$(points_per_dim)_tau$(tau).png"
-    savefig(ref, name)
-
-    @info "File saved: " name
-end
-
-
-"""
 Save figures.
 """
 function save_figure(plot_ref, results_path, plot_title; extension=".png" )
@@ -29,4 +14,24 @@ Save betti curves.
 function save_betti(plot_ref, results_path, plot_title)
     full_title =  "betti_curves_"*plot_title;
     save_figure(plot_ref, results_path, full_title)
+end
+
+"""
+Save figures with set of parameters given as 'kwargs'.
+"""
+function save_figure_with_params(plot_reference, results_path; extension=".png", kwargs... )
+    plot_title = ""
+    kwargs_kyes = keys(kwargs)
+    kwargs_vals = values(kwargs)
+
+    total_params = size(collect(kwargs_vals),1)
+
+    for param = 1:total_params
+        plot_title *= "$(kwargs_kyes[param])_$(kwargs_vals[param])_"
+    end
+
+    full_path = results_path*plot_title*extension
+
+    # savefig(plot_ref, full_path)
+    @info "File saved as: " full_path
 end
